eea87b62a43486a3cbdc03128dc5aa8d51a71b62
Darren De Doncker <s213402432@Mandela.ac.za>
2023-02-08 14:21:03 +0200

    main index, some style elements, starting classes

---
+++ game.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('game-canvas');
+const ctx = canvas.getContext('2d');
+
+const gameContainer = document.getElementById('game-container');
+
+const flappyImg = new Image();
+flappyImg.src = 'assets/flappy_dunk.png';
